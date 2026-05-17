cc7d2f4a9e5f7dcf5c54f28e35e925345c4bddd4
Mosamagdy <mosaelwayly@gmail.com>
2026-05-17 15:13:28 +0300

    mm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://smart-energy-backend-production-5668.up.railway.app'
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
 };
